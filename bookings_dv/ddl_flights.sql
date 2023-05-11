c31a3cc6c94b4e0a77c18a284d2859b3acbaa3bb
Сергей Логинов <lalogin@MacBook-Air-Sergej.local>
2023-05-11 11:49:00 +0300

    ddl changes, gitignore, readme

--- bookings_dv/ddl_flights.sql
+++ bookings_dv/ddl_flights.sql
@@ -1,13 +1,24 @@
-CREATE TABLE h_flights AS (
+CREATE TABLE sl_dv.h_flights AS (
 	SELECT md5((flight_no || scheduled_departure)::bytea) AS flights_hash_key,
 		   CAST(CURRENT_TIMESTAMP(0) AS timestamp) AS load_date,
 		   'my_soure' AS source_load,
 		   flight_no,
-		   scheduled_departure,
-		   flight_id
-	FROM flights
+		   scheduled_departure
+	FROM bookings.flights
 );
 ALTER TABLE sl_dv.h_flights ADD PRIMARY KEY (flights_hash_key);
+
+CREATE TABLE sl_dv.s_flights_flight_id AS (
+	SELECT md5((flight_no || scheduled_departure)::bytea) AS flights_hash_key,
+		   CAST(CURRENT_TIMESTAMP(0) AS timestamp) AS load_date,
+		   '9999-12-31 23:59:59' :: timestamp AS load_end_date,
+		   'my_soure' AS source_load,
+		   flight_id
+	FROM bookings.flights
+);
+
+ALTER TABLE sl_dv.s_flights_flight_id ADD PRIMARY KEY (flights_hash_key, load_date);
+ALTER TABLE sl_dv.s_flights_flight_id ADD FOREIGN KEY (flights_hash_key) REFERENCES h_flights(flights_hash_key);
 
 CREATE TABLE sl_dv.s_flights_status AS (
 	SELECT md5((flight_no || scheduled_departure)::bytea) AS flights_hash_key,
@@ -19,7 +30,6 @@ CREATE TABLE sl_dv.s_flights_status AS (
 );
 
 ALTER TABLE sl_dv.s_flights_status ADD PRIMARY KEY (flights_hash_key, load_date);
-ALTER TABLE sl_dv.s_flights_status ADD UNIQUE (flights_hash_key);
 ALTER TABLE sl_dv.s_flights_status ADD FOREIGN KEY (flights_hash_key) REFERENCES h_flights(flights_hash_key);
 
 CREATE TABLE sl_dv.s_flights_scheduled_arrival AS (
@@ -27,7 +37,7 @@ CREATE TABLE sl_dv.s_flights_scheduled_arrival AS (
 		   CAST(CURRENT_TIMESTAMP(0) AS timestamp) AS load_date,
 		   '9999-12-31 23:59:59' :: timestamp AS load_end_date,
 		   'my_soure' AS source_load,
-		   actual_departure
+		   actual_arrival
 	FROM bookings.flights
 );
 
@@ -43,17 +53,17 @@ CREATE TABLE sl_dv.s_flights_actual_departure AS (
 	FROM bookings.flights
 );
 
-ALTER TABLE sl_dv.s_flights_actual_arrival ADD PRIMARY KEY (flights_hash_key, load_date);
-ALTER TABLE sl_dv.s_flights_actual_arrival ADD FOREIGN KEY (flights_hash_key) REFERENCES h_flights(flights_hash_key);
+ALTER TABLE sl_dv.s_flights_actual_departure ADD PRIMARY KEY (flights_hash_key, load_date);
+ALTER TABLE sl_dv.s_flights_actual_departure ADD FOREIGN KEY (flights_hash_key) REFERENCES h_flights(flights_hash_key);
 
 CREATE TABLE sl_dv.s_flights_actual_arrival AS (
 	SELECT md5((flight_no || scheduled_departure)::bytea) AS flights_hash_key,
 		   CAST(CURRENT_TIMESTAMP(0) AS timestamp) AS load_date,
 		   '9999-12-31 23:59:59' :: timestamp AS load_end_date,
 		   'my_soure' AS source_load,
-		   actual_departure
+		   actual_arrival
 	FROM bookings.flights
 );
 
-ALTER TABLE sl_dv.s_flights_actual_departure ADD PRIMARY KEY (flights_hash_key, load_date);
-ALTER TABLE sl_dv.s_flights_actual_departure ADD FOREIGN KEY (flights_hash_key) REFERENCES h_flights(flights_hash_key);
+ALTER TABLE sl_dv.s_flights_actual_arrival ADD PRIMARY KEY (flights_hash_key, load_date);
+ALTER TABLE sl_dv.s_flights_actual_arrival ADD FOREIGN KEY (flights_hash_key) REFERENCES h_flights(flights_hash_key);
